fe869a63579df7e6f876767b57dc39f469941992
David Coallier <david@echolibre.com>
2009-06-10 22:21:40 +0100

    The start of the library

---
+++ src/Exception.php
@@ -0,0 +1,5 @@
+<?php
+namespace echolibre\google_wave;
+
+class Exception extends \Exception {}
+class RangeException extends \RangeException {}
---
+++ src/Model.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace echolibre\google_wave\Model;
+
+const ROOT_WAVELET_ID_SUFFIX = '!conv+root';
+
+class WaveData
+{
+    protected $id = null;
+    protected $waveletIds = array();
+}
+
+class Wave
+{
+    private $data;
+    
+    public function __construct(\echolibre\google_wave\WaveData $data)
+    {
+        $this->data = $data;
+    }
+    
+    public function getId()
+    {
+        return $this->data->id;
+    }
+    
+    public function getWaveletIds()
+    {
+        return $this->data->waveletIds;
+    }
+}
+
+class WaveletData
+{
+    const JAVA_CLASS = 'com.google.wave.api.impl.WaveletData';
+    
+    private $variables = array();
+    
+    public function __construct()
+    {
+        $this->creator = null;
+        $this->creationTime = 0;
+        $this->dataDocuments = new \stdClass();
+        $this->lastModifiedTime = 0;
+        $this->participants = array();
+        $this->rootBlipId = null;
+        $this->title = 'f';
+        $this->version = 0;
+        $this->waveId = null;
+        $this->waveletId = null;
+    }
+    
+    public function __set($key, $value)
+    {
+        $this->variables[$key] = $value;
+    }
+    
+    public function __get($key) 
+    {
+        if (isset($this->variables[$key])) {
+            return $this->variables[$key];
+        }
+        
+        return false;
+    }
+}
+
+class Wavelet
+{
+    private $data;
+    public function __construct(\echolibre\google_wave\Model\WaveletData $data)
+    {
+        $this->data = $data;
+    }
+    
+    public function __get($key)
+    {
+        if ($this->data->{$key} !== false) {
+            return $this->data->{$key};
+        }
+        
+        return false;
+    }
+}
